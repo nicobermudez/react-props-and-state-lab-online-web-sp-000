cc396d267f5e0536bf346e5116cd10eac6d86b89
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-26 18:30:32 +0000

    Automatically backed up by Learn

--- src/components/Pet.js
+++ src/components/Pet.js
@@ -10,7 +10,7 @@ class Pet extends React.Component {
             {this.props.pet.gender === 'female' ?'♀' : '♂'}
           </a>
           <div className="meta">
-            <span className="date">PET TYPE</span>
+            <span className="date">{this.props.pet.type}</span>
           </div>
           <div className="description">
             <p>Age: PET AGE</p>
